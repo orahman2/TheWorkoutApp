86559573f4b39a40083b9660fe8db0f179aa64b3
Omar <omarrahman96@gmail.com>
2016-08-21 21:15:26 +0400

    added fragment and added workout identifier

---
+++ app/src/main/java/com/example/android/theworkoutapp/WorkoutDetailFragment.java
@@ -0,0 +1,34 @@
+package com.example.android.theworkoutapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+
+//    workout identifier
+    private long workoutID;
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+    public void setWorkoutID(long workoutID) {
+        this.workoutID = workoutID;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+}
